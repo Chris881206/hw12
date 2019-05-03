9ba09c87c661ef6e703f7efe487a3c53b6e26609
Chris881206 <48573768+Chris881206@users.noreply.github.com>
2019-05-03 15:54:31 +0800

    Add files via upload

---
+++ HW12.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int x=5;
+	cout<<endl;
+    cout<<"­Ë¤T¨¤§Î"<<endl;
+    for(int i=1;i<=x;i++){
+        for(int y=1;y<i;y++){
+            cout<<" ";
+        }
+
+        for(int y=x;y>=i;y--){
+            cout<<"*";
+        }
+
+        cout<<endl;
+    }
+    system("pause");
+    return 0;
+	
+}
